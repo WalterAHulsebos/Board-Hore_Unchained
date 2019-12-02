b3d83867cb850f9c1e5f5de140c909d74e9f9aeb
Walter Haynes <walter-haynes@outlook.com>
2019-12-02 21:39:18 +0100

    Added ArealGravity

--- Assets/[Source]/Scripts/Core/PlayerSystems/Movement/Physics/ArealGravity.cs
+++ Assets/[Source]/Scripts/Core/PlayerSystems/Movement/Physics/ArealGravity.cs
@@ -1,18 +1,44 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+
 using UnityEngine;
 
-public class ArealGravity : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+using CommonGames.Utilities.Extensions;
 
-    // Update is called once per frame
-    void Update()
+namespace Core.PlayerSystems.Movement
+{
+    public class ArealGravity : VehicleBehaviour
     {
+        #region Variables
+
+        [SerializeField] private float airGravityMultiplier = 2f;
+
+        private float _defaultGravity = default;
         
+        #endregion
+
+        #region Methods
+        
+        protected override void Start()
+        {
+            base.Start();
+
+            _defaultGravity = UnityEngine.Physics.gravity.y;
+        }
+
+        private void FixedUpdate()
+        {
+            if(_vehicle.Grounded)
+            {
+                UnityEngine.Physics.gravity = new Vector3(0, _defaultGravity, 0);
+            }
+            else
+            {
+                UnityEngine.Physics.gravity = new Vector3(0, _defaultGravity * airGravityMultiplier, 0);
+            }
+        }
+
+        #endregion
     }
 }
